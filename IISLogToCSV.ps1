9eef9d6ee9530c265c030293fc6f00cab9e34bcc
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2021-03-23 17:39:07 -0400

    Create IISLogToCSV.ps1

---
+++ IISLogToCSV.ps1
@@ -0,0 +1,40 @@
+################################# Input to be changed ###########################################
+$inputPath = "<folder which contains .log files>"
+
+
+########################### Change only if needed in environment ################################
+$pathToLogParserExe= "C:\Program Files (x86)\Log Parser 2.2\logparser.exe"
+
+################################ DO NOT CHANGE ##################################################
+$outExt = ".csv"
+Get-ChildItem $inputPath -Filter *.log | 
+Foreach-Object {
+    $inFile = $_.FullName
+    "Starting " + $inFile
+    $outFile =  $_.FullName -replace "\.log",".csv"
+    $cmd = "SELECT date, time, s-ip, cs-method, cs-uri-stem, cs-uri-query, s-port, cs-username, c-ip, cs(User-Agent) as cs-user-agent, sc-status, sc-substatus, sc-win32-status,sc-bytes,cs-bytes, time-taken INTO '"+$outFile+"' FROM '"+$inFile+"'"
+    
+    $output =  & $pathToLogParserExe -i:W3C -o:csv $cmd | Out-String
+    
+    "Output of first conversion " +$output.Length + ". If it is 0, it will rerun to include missing fields with default values. Make sure sc-bytes or cs-bytes columns available in IISLogs."
+ 
+    # Hack in case the log file dont have all fields it will rerun to include those with default values. Mainly it happens for sc-bytes, cs-bytes as those are not enabled by default.
+    # TODO move the default logic to PowerBI if possible.
+ 
+    if ($output.Length -eq 0) {
+        #There may be missing columns such as sc-bytes, cs-bytes. Rerun without those fields & Add default value 0
+        Write-Information "Converting again"
+        $cmd = "SELECT date, time, s-ip, cs-method, cs-uri-stem, cs-uri-query, s-port, cs-username, c-ip, cs(User-Agent) as cs-user-agent, sc-status, sc-substatus, sc-win32-status,0 as sc-bytes,0 as cs-bytes, time-taken INTO '"+$outFile+"' FROM '"+$inFile+"'"
+        
+        $output =  & $pathToLogParserExe -i:W3C -o:csv $cmd | Out-String
+        
+        "Output of rerun is " + $output.Length
+        if($output.Length -eq 0){
+            Write-Error "Not able to convert. Please make sure the IIS log file is W3C format and minimum columns date, time, s-ip, cs-method, cs-uri-stem, cs-uri-query, s-port, cs-username, c-ip, cs(User-Agent), sc-status, sc-substatus, sc-win32-status, time-taken are included"
+        }
+    } else {
+        "Completed file " + $inFile
+    }
+    "-"*100
+}
+"Completed all files"
